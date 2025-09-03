7991cd15433424bb9bda0e1bb9861e523ec9a627
altinalivings <altinalivings@gmail.com>
2025-09-04 00:59:54 +0530

    qw

--- src/components/PageViewTracker.js
+++ src/components/PageViewTracker.js
@@ -1,34 +1,37 @@
 "use client";
 
-import { Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
+import { Suspense } from "react";
 
-export default function PageViewTracker() {
+function PageViewInner() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = pathname + searchParams.toString();
-
-    // GA4 + Ads
-    if (window.gtag) {
-      window.gtag("event", "page_view", {
-        page_path: url,
-        page_location: window.location.href,
-      });
+    if (typeof window !== "undefined") {
+      if (window.gtag) {
+        window.gtag("event", "page_view", {
+          page_path: pathname,
+          page_location: window.location.href,
+        });
+      }
+      if (window.fbq) {
+        window.fbq("track", "PageView");
+      }
+      if (window.lintrk) {
+        window.lintrk("track", { conversion_id: 515682278 });
+      }
     }
-
-    // FB Pixel
-    if (window.fbq) {
-      window.fbq("track", "PageView");
-    }
-
-    // LinkedIn
-    if (window.lintrk) {
-      window.lintrk("track", { conversion_id: 515682278 });
-    }
-  }, [pathname, searchParams]    </Ssuspen>);
+  }, [pathname, searchParams]);
 
   return null;
+}
+
+export default function PageViewTracker() {
+  return (
+    <Suspense fallback={null}>
+      <PageViewInner />
+    </Suspense>
+  );
 }
