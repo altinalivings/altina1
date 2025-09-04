02cfb046969cb2845e077f41c5e218776d791e6e
altinalivings <altinalivings@gmail.com>
2025-09-04 21:13:04 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 // Server Component (no "use client")
+import "../styles/globals.css";
+
 import Script from "next/script";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
